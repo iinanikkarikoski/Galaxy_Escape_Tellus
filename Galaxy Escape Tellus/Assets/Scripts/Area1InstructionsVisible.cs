e34f5a417c54cfb8c269b27895aae37aff32249f
iiriskivela <98057737+iiriskivela@users.noreply.github.com>
2024-12-04 17:49:21 +0200

    kaikkee pientä taas

--- Galaxy Escape Tellus/Assets/Scripts/Area1InstructionsVisible.cs
+++ Galaxy Escape Tellus/Assets/Scripts/Area1InstructionsVisible.cs
@@ -3,22 +3,27 @@ using UnityEngine;
 public class Area1InstructionsVisible : MonoBehaviour
 {
     public GameObject collectiblesPlane1; 
-    
     //public GameObject collectiblesPlane2;
     //public GameObject collectiblesPlane3;
     public GameObject instructionsPlane1;
     //public GameObject instructionsPlane2;
     //public GameObject instructionsPlane3;
 
+    private bool hasActivated1 = false; // To track if the plane has already been shown
+    //private bool hasActivated2 = false; // To track if the plane has already been shown
+    //private bool hasActivated3 = false; // To track if the plane has already been shown
+
     void OnTriggerEnter(Collider other)
     {
         // Check if the object entering the trigger is the player
-        if (other.CompareTag("player 1"))
+        if (other.CompareTag("player 1") && !hasActivated1)
         {
+            hasActivated1 = true;
             // Activate the instruction plane
             if (collectiblesPlane1 != null)
             {
                 collectiblesPlane1.SetActive(true);
+                // tähän jotain että kun kaikki kerätty niin pois päältä
             }
             else
             {
@@ -26,7 +31,8 @@ public class Area1InstructionsVisible : MonoBehaviour
             }
             if (instructionsPlane1 != null)
             {
-                instructionsPlane1.SetActive(true);
+                instructionsPlane1.SetActive(true); // Activate the instruction plane
+                //instructionsPlane1.SetActive(false); jotain tähän että milloin menee pois päältä
             }
             else
             {
